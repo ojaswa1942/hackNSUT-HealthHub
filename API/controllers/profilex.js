80143155d65b71cd1a8d9baa3b7e1b34581311e0
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-03-31 02:18:45 +0530

    Signin/Register for Patient & Doc

--- API/controllers/profilex.js
+++ API/controllers/profilex.js
@@ -1,39 +1,46 @@
  const handleProfile=(req,res,db)=>{
 	const email = req.email;
 	let user={}, events;
- 	db.select('*').from('users').where({email})
-	.then(user => {
-		if(user.length){
-			const {ifid} = user[0];
-			db('event_reg')
-			.join('payment', 'event_reg.teamid', '=', 'payment.teamid')
-			.join('events', 'events.eid', '=', 'event_reg.eid')
-			.select('event_reg.eid', 'events.ename', 'events.category', 'payment.teamid', 'events.fee', 'payment.status')
-			.where('event_reg.ifid', '=', ifid)
-			.then(registrations => {
-				var subquery = db('event_reg').where({ifid}).select('teamid');
-				db('event_reg')
-				.join('users','users.ifid', '=', 'event_reg.ifid')
-				.select('event_reg.eid', 'users.ifid', 'users.name', 'event_reg.teamid')
-				.where('event_reg.teamid', 'in', subquery)
-				.then(teamData =>{
-					db('easter_redeem')
-					.sum({total: 'score'})
-					.where({ifid})
-					.then(userScore => {
+ 	db.select('*').from('patients').where({email})
+	.then(data => {
+		if(data.length){
+			return db('reports')
+			.join('doctors', 'reports.docid', '=', 'doctors.docid')
+			.select('reports.date', 'reports.title', 'reports.hash', 'doctors.name', 'doctors.email')
+			.where({pid: data[0].pid})
+			.then(reports =>{
+				let userData = {
+					isUser: true,
+					isDoc: false,
+					userReports: reports,
+					userInfo: data[0]
+				}
+				res.status(200).json(userData);						
+			})
+		}
+		else{
+ 			db.select('*').from('doctors').where({email})
+ 			.then(doc => {
+ 				if(doc.length){
+ 					return db('reports')
+					.join('patients', 'reports.pid', '=', 'patients.pid')
+					.select('reports.date', 'reports.title', 'reports.hash', 'patients.pid', 'patients.name', 'patients.email')
+					.where({docid: doc[0].docid})
+					.then(reports =>{
 						let userData = {
-							userEventReg: registrations,
-							userTeams: teamData,
-							user: user[0],
-							userScore: userScore[0].total
+							isUser: false,
+							isDoc: true,
+							userReports: reports,
+							userInfo: doc[0]
 						}
 						res.status(200).json(userData);						
 					})
-				})
-			})
+ 				}
+ 				else{
+					res.status(404).json('No such user');
+ 				}
+ 			})
 		}
-		else
-			res.status(404).json('No such user');
 	})
 	.catch(err => {console.log('Profilex', err); res.status(400).json('Something is wrong');});
 }
