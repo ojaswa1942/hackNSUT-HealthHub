ada8377eae1fc5088df688773f12ad6d7889aebc
Prem Kumar <prmsrswt@gmail.com>
2019-03-31 03:12:57 +0530

    Implemented frontend for Report upload by doctor

--- src/Components/Doctor/Doctor.js
+++ src/Components/Doctor/Doctor.js
@@ -7,6 +7,7 @@ import '../../assets/css/dashboard.css';
 //import headers from "../../assets/logo/headers.png";
 import {Loader} from '../_Loader/Loader';
 import ReportCard from '../Patient/ReportCard';
+import Modal from 'react-awesome-modal';
 
 class Doctor extends Component {
 
@@ -17,7 +18,9 @@ class Doctor extends Component {
 	    redirect: false,
 	    error: false,
 	    errorMessage: '',
-        visibleModal: false
+        visibleModal: false,
+        visibleSuccessModal: false,
+        hash: ''
 	};
   }
 
@@ -34,6 +37,39 @@ class Doctor extends Component {
 	} else this.setState({loading: false});
   }
 
+  toggleModal = () =>{
+  	this.setState({
+  		visibleModal: true
+  	});
+  }
+
+  handleUpload = () =>{
+  	var formData = new FormData();
+  	formData.append("email", document.getElementById("up_email").value);
+  	formData.append("file", document.getElementById("up_file").files[0]);
+
+  	let err = false;
+	fetch('/api/upload', {
+		method: 'post',
+		//headers: {'Content-type': 'application/json'},
+		body: formData
+	})
+	.then(response => {
+		if(response.status !== 200)
+			err = true;
+		return response.json()
+	})
+	.then((user) => {
+		if(err)
+			throw(user);
+		console.log(user);
+		this.setState({
+			hash: user.hash,
+			visibleSuccessModal: true
+		});
+	})
+	.catch(err => {this.setState({receivedError: true, responseMessage: err})});
+  }
 
   render() {
   	const { loading, redirect } = this.state;
@@ -69,6 +105,34 @@ class Doctor extends Component {
 		  			<Redirect to='/login' />
 	  			:
 	  				<div className="profile-content">
+	  					<Modal 
+			                visible={this.state.visibleModal}
+			                effect="fadeInDown"
+			                onClickAway={() => this.setState({visibleModal: false})}
+			            >
+			                <div id="contact">
+						      <div className="inputContainer">
+						        <input type="text" name="email" className="inputField" id="up_email" required />
+						        <label className="inputLabel">Email</label>
+						      </div>
+						      <div className="inputContainer">
+						        <input type="file" name="file" className="inputField" id="up_file" required />
+						        <label className="inputLabel">Report</label>
+						      </div>
+						      <div className="buttonContainer dim pointer">
+						        <button id="submitButton" className="pointer" onClick={this.handleUpload}>Submit</button>
+						      </div>
+			                </div>
+			            </Modal>
+			            <Modal 
+			                visible={this.state.visibleSuccessModal}
+			                effect="fadeInDown"
+			                onClickAway={() => this.setState({visibleSuccessModal: false})}
+			            >
+			                <div className='black f5 flex flex-column items-center pa3 bg-near-gray'>
+			                    <div className='mb2'>Success!</div><div className='t mh2'> Hash: <b>{this.state.hash}</b></div>
+			                </div>
+			            </Modal>
 					    <div className="profile-headin">
 					    	<h2 className='mv'>DOCTOR PROFILE</h2>
 			  				<h3 className='mv3 wellc'>Welcome {userInfo.name},</h3>
@@ -76,6 +140,9 @@ class Doctor extends Component {
 			  					<div className="detailsCard">
 			  						<div className="detailsD"><b>Mobile:</b> {userInfo.mobile}</div>
 			  						<div className="detailsD"><b>Email:</b> {userInfo.email}</div>
+			  					</div>
+			  					<div className="detailsCard">
+			  						<div className="uploadD" onClick={this.toggleModal}>Upload new report.</div>
 			  					</div>
 			  					<div className="detailsD">
 			  						<br />
