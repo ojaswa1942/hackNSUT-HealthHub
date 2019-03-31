1583c86963643b9d4b503bc8711753f7bedfeee5
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-03-31 09:23:52 +0530

    Upload change

--- API/controllers/upload.js
+++ API/controllers/upload.js
@@ -17,6 +17,10 @@ const handleUpload = (req,res, db) =>{
 	    });
 	}
   console.log(req.file);
+  console.log(req.body.email);
+  const email = req.body.email;
+  const did = req.body.did;
+  const title = req.body.title;
 
   const fileSize = req.file.size;
   if (fileSize > MAX_SIZE) {
@@ -27,20 +31,50 @@ const handleUpload = (req,res, db) =>{
     });
   }
 
-  const data = fs.readFileSync(req.file.path);
-  return ipfs.add(data, (err, files) => {
-    console.log(files);
-    //fs.unlink(req.file.path);
-    if (files) {
-      return res.json({
-        hash: files[0].hash,
-        link: 'https://ipfs.premsarswat.me/ipfs/'+files[0].hash
+  db.select('*').from('patients').where({email})
+  .then(data => {
+    if(data.length){
+    const data = fs.readFileSync(req.file.path);
+      return ipfs.add(data, (err, files) => {
+        console.log(files);
+        //fs.unlink(req.file.path);
+        if (files) {
+          db.insert({
+            pid: data[0].pid, 
+            docid: did,
+            title: title,
+            hash: files[0].hash
+          })
+          .into('reports')
+          .then(() =>{
+            return db('reports')
+            .join('patients', 'reports.pid', '=', 'patients.pid')
+            .select('reports.date', 'reports.title', 'reports.hash', 'patients.pid', 'patients.name', 'patients.email')
+            .where({docid: did})
+            .then(reports =>{
+              let userData = {
+                isUser: false,
+                isDoc: true,
+                userReports: reports,
+                userInfo: doc[0],
+                hash: files[0].hash,
+                link: 'https://ipfs.premsarswat.me/ipfs/'+files[0].hash
+              }
+              res.status(200).json(userData);
+            })
+          })
+        }
+        return res.status(500).json({
+          error: err,
+        });
       });
     }
-    return res.status(500).json({
-      error: err,
-    });
-  });
+    else{
+      res.status(404).json('No such user');
+    }
+  })
+
+  
 }
 
 module.exports = {
